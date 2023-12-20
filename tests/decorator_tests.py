eb7c287268853f8e9272f08746d1a952ccd7046b
German Espinosa <germanespinosa@gmail.com>
2023-12-20 14:58:08 -0600

    added tests for parse_parameters decorator

---
+++ tests/decorator_tests.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+sys.path.append('..')
+from jsonbind import Serialization
+
+
+class BytesTests(unittest.TestCase):
+    def test_bytes_serialization(self):
+        self.assertEqual(Serialization.serialize(b"Hello"), '"SGVsbG8="')
+
+    def test_bytes_deserialization(self):
+        self.assertEqual(Serialization.deserialize('"SGVsbG8="', bytes), b"Hello")
+
+
+if __name__ == '__main__':
+    unittest.main()
