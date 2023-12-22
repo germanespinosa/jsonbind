1472c453b2c3f123db0897780395e49b53991d74
German <germanespinosa@gmail.com>
2023-12-22 11:02:01 -0600

    refactor, clean up

--- jsonbind/core/serialization.py
+++ jsonbind/core/serialization.py
@@ -1,6 +1,6 @@
 import typing
 import json
-from ..core.type_binding import Bindings, JsonTypes
+from ..core.type_binding import Bindings
 
 
 class Serialization:
--- jsonbind/special/string.py
+++ jsonbind/special/string.py
@@ -1,19 +1,18 @@
 from ..core.serialization import Serialization
 
 
-class JsonString(str):
+class String(str):
 
-    def __new__(cls, string=""):
-        if string:
+    def __new__(cls, json_string=""):
+        if json_string:
             try:
-                o = Serialization.deserialize(json_string=string)
+                o = Serialization.deserialize(json_string=json_string)
                 instance = super().__new__(cls, str(o))
                 setattr(instance, "value", o)
             except:
-                instance = super().__new__(cls, string)
+                instance = super().__new__(cls, json_string)
                 setattr(instance, "value", None)
         else:
             instance = super().__new__(cls)
         return instance
-
 
